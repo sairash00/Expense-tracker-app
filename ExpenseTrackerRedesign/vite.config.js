0d66a8e50ab1699c77d7029b70cbc7cbe510ad0b
sairash00 <sairash7248@gmail.com>
2024-09-07 09:48:29 +0545

    frontend updated

--- ExpenseTrackerRedesign/vite.config.js
+++ ExpenseTrackerRedesign/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://expense-tracker-app-backend-rose.vercel.app/',
         changeOrigin: true,
       },
     },
